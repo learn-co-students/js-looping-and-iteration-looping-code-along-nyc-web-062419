7923ace7e71517e40e54539d3b4a0373249dae09
IanGrubb <yangrubb@gmail.com>
2019-08-03 20:41:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function countdown (count) {
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+}
+
+function writeCards (names, kind) {
+    const cards = []
+    for (var i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${kind} gift!`)
+    }
+    return cards
+}
