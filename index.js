f415aa61f0f2ff9e27dc08417509d6d73ebdf638
KConlon13 <KyleConlon13@gmail.com>
2019-08-04 17:48:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    let messages = [];
+    for (let i=0; i< array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countdown(num) {
+    while (num > -1) {
+        console.log(num--)
+    }
+}
