8e8d53a3bcfd1bd95ec909b2e5edebb54303aac6
Yana <Yana.Loginova@cix.csi.cuny.edu>
2019-08-05 11:44:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+let writeCards = (name_array, event) => {
+  let new_array = [];
+  for (let i=0; i<name_array.length;i++){
+  new_array.push(`Thank you, ${name_array[i]}, for the wonderful ${event} gift!`)
+}
+return new_array;
+}
+
+let countdown = (int) => {
+  while (int > 0) {
+    console.log(int--);
+
+  }
+  console.log(int)
+}
