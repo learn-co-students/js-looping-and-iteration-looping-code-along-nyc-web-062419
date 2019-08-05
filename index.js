03f91f5fd8855ea5ce617499558f94a35709801c
joe lazarz <jlazarz87@gmail.com>
2019-08-04 20:42:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(array,surprise){
+    let thankYouCards = []
+    for (let i = 0; i < array.length; i++) {
+        thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${surprise} gift!`)
+    }
+    return thankYouCards;
+}
+
+function countdown(countdown) {
+    while (countdown > 0) {
+      console.log(countdown);
+      countdown = countdown - 1;
+    }
+    console.log(countdown);
+  }
